2cc1b7e2e8e7310767cbdcdd62fbedbf7899a593
mohammed <Mohammed.Lacarne@ergo.de>
2025-06-18 15:48:46 +0200

    Add OIDC for Github Actions

---
+++ terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.3.0"
+    }
+    archive = {
+      source = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = "~> 1.3"
+}
+
+provider "aws" {
+  region  = "eu-central-1"
+  profile = "terraform"
+}
